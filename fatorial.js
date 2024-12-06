9ca7e6738ea4ce37bd6fe53921acb0f1100a7a91
Gioobarros <153088687+Gioobarros@users.noreply.github.com>
2024-12-06 03:33:36 +0000

    :card_file_box: chore: Adição das dependências do typescript

--- fatorial.js
+++
@@ -1,4 +0,0 @@
-function fatorial(n) {
-  return n <= 1 ? 1 : n * fatorial(n - 1);
-}
-module.exports = fatorial;
--- fatorial.test.js
+++
@@ -1,9 +0,0 @@
-const fatorial = require('./fatorial');
-
-test('Calcula o fatorial de 5, igual a 120.', () => {
-  expect(fatorial(5)).toBe(120);
-});
-
-test('Calcula o fatorial de 10, igual a 3628800.', () => {
-  expect(fatorial(10)).toBe(3628800);
-});
